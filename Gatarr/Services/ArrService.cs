f5673043d37b4bb0076e9ce3c932726b3951c095
mcbtay <mcbtay@gmail.com>
2022-04-24 23:16:04 -0400

    adding ArrService, a base class for the other services

---
+++ Gatarr/Services/ArrService.cs
@@ -0,0 +1,31 @@
+﻿namespace Gatarr.Services
+{
+    public class ArrService
+    {
+        protected readonly IConfiguration Configuration;
+        protected readonly HttpClient HttpClient;
+
+        protected readonly string Name;
+
+        protected readonly string Host;
+        protected readonly string Port;
+        protected readonly string ApiKey;
+
+        protected ArrService(
+            IConfiguration configuration,
+            IHttpClientFactory httpClientFactory,
+            string name)
+        {
+            Configuration = configuration;
+            Name = name;
+
+            Host = Configuration[$"{Name}:Host"];
+            Port = Configuration[$"{Name}:Port"];
+            ApiKey = Configuration[$"{Name}:ApiKey"];
+
+            HttpClient = httpClientFactory.CreateClient();
+            HttpClient.BaseAddress = new Uri($"http://{Host}:{Port}:/api/v3/");
+            HttpClient.DefaultRequestHeaders.Add("X-Api-Key", ApiKey);
+        }
+    }
+}
--- Gatarr/Services/RadarrService.cs
+++ Gatarr/Services/RadarrService.cs
@@ -9,40 +9,26 @@ namespace Gatarr.Services
         Task<List<MovieResource>> GetMovies();
     }
 
-    public class RadarrService : IRadarrService
+    public class RadarrService : ArrService, IRadarrService
     {
-        private readonly IConfiguration _configuration;
-        private readonly HttpClient _httpClient;
-
         public RadarrService(
             IConfiguration configuration,
             IHttpClientFactory httpClientFactory)
+            : base(configuration, httpClientFactory, "Radarr")
         {
-            _configuration = configuration;
 
-            var host = _configuration["Radarr:Host"];
-            var port = _configuration["Radarr:Port"];
-            var apiKey = _configuration["Radarr:ApiKey"];
-
-            //var host = "192.168.1.222";
-            //var port = "7878";
-            //var apiKey = "a99bd0920631402ca9f7f86bee1c1e12";
-
-            _httpClient = httpClientFactory.CreateClient();
-            _httpClient.BaseAddress = new Uri($"http://{host}:{port}/api/v3/");
-            _httpClient.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
         }
 
         public async Task<HttpResponseMessage> GetLog()
         {
-            return await _httpClient.GetAsync("log");
+            return await HttpClient.GetAsync("log");
         }
 
         public async Task<List<MovieResource>> GetMovies()
         {
             var movies = new List<MovieResource>();
 
-            var response = await _httpClient.GetAsync("movie");
+            var response = await HttpClient.GetAsync("movie");
 
             if (response?.Content == null || !response.IsSuccessStatusCode)
             {
