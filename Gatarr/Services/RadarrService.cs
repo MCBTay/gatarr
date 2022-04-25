2a4cc5eef62238186f34a873249c97d735f93d5e
mcbtay <mcbtay@gmail.com>
2022-04-24 20:23:38 -0400

    adding RadarrService, a simple service to interact with Radarr's api

---
+++ Gatarr/Services/RadarrService.cs
@@ -0,0 +1,22 @@
+﻿namespace Gatarr.Services
+{
+    public interface IRadarrService
+    {
+        Task<HttpResponseMessage> GetLog();
+    }
+
+    public class RadarrService : IRadarrService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RadarrService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> GetLog()
+        {
+            return await _httpClient.GetAsync("log");
+        }
+    }
+}
