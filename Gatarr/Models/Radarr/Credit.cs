58a74856f042d7c66d4ecfce1723f4566f2c14e7
mcbtay <mcbtay@gmail.com>
2022-04-25 22:46:06 -0400

    add credit mdoel

---
+++ Gatarr/Models/Radarr/Credit.cs
@@ -0,0 +1,17 @@
+﻿namespace Gatarr.Models.Radarr
+{
+    public class Credit
+    {
+        public string personName { get; set; }
+        public string creditTmdbId { get; set; }
+        public int personTmdbId { get; set; }
+        public int movieId { get; set; }
+        public List<Image> images { get; set; }
+        public string character { get; set; }
+        public int order { get; set; }
+        public string type { get; set; }
+        public int id { get; set; }
+        public string department { get; set; }
+        public string job { get; set; }
+    }
+}
