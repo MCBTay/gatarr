3116a0ac1fdd20a999a58424e386bd07f00029fb
mcbtay <mcbtay@gmail.com>
2022-04-26 18:53:21 -0400

    adding models for history

---
+++ Gatarr/Models/Radarr/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace Gatarr.Models.Radarr
+{
+    public class Data
+    {
+        public string additionalProp1 { get; set; }
+        public string additionalProp2 { get; set; }
+        public string additionalProp3 { get; set; }
+    }
+}
---
+++ Gatarr/Models/Radarr/MovieHistory.cs
@@ -0,0 +1,18 @@
+﻿namespace Gatarr.Models.Radarr
+{
+    public class MovieHistory
+    {
+        public int id { get; set; }
+        public int movieId { get; set; }
+        public string sourceTitle { get; set; }
+        public List<Language> languages { get; set; }
+        public Quality quality { get; set; }
+        public List<CustomFormat> customFormats { get; set; }
+        public bool qualityCutoffNotMet { get; set; }
+        public DateTime date { get; set; }
+        public string downloadId { get; set; }
+        public string eventType { get; set; }
+        public Data data { get; set; }
+        public MovieResource movie { get; set; }
+    }
+}
