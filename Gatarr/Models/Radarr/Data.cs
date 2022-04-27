f1ba0820f9a4fbdfc15665c18f9ebdd79540fc87
mcbtay <mcbtay@gmail.com>
2022-04-26 21:03:40 -0400

    updating to match the payloads i'm actually getting back

--- Gatarr/Models/Radarr/Data.cs
+++ Gatarr/Models/Radarr/Data.cs
@@ -2,8 +2,28 @@
 {
     public class Data
     {
-        public string additionalProp1 { get; set; }
-        public string additionalProp2 { get; set; }
-        public string additionalProp3 { get; set; }
+        public string sourcePath { get; set; }
+        public string sourceRelativePath { get; set; }
+        public string path { get; set; }
+        public string relativePath { get; set; }
+        public string releaseGroup { get; set; }
+        public string fileId { get; set; }
+        public string droppedPath { get; set; }
+        public string importedPath { get; set; }
+        public string downloadClient { get; set; }
+        public string downloadClientName { get; set; }
+        public string indexer { get; set; }
+        public string nzbInfoUrl { get; set; }
+        public string age { get; set; }
+        public string ageHours { get; set; }
+        public string ageMinutes { get; set; }
+        public DateTime? publishedDate { get; set; }
+        public string size { get; set; }
+        public string downloadUrl { get; set; }
+        public string guid { get; set; }
+        public string tmdbId { get; set; }
+        public string protocol { get; set; }
+        public string indexerFlags { get; set; }
+        public string indexerId { get; set; }
     }
 }
