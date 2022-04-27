a2eb1f6cd0481b51ee055872e7533260699da49a
mcbtay <mcbtay@gmail.com>
2022-04-27 19:50:37 -0400

    add method to getseries and getepisodes

--- Gatarr/Services/SonarrService.cs
+++ Gatarr/Services/SonarrService.cs
@@ -7,6 +7,8 @@ namespace Gatarr.Services
     {
         Task<HttpResponseMessage> GetLog();
         Task<List<Series>> GetSeries();
+        Task<Series> GetSeries(string id);
+        Task<List<Episode>> GetEpisodes(string id);
     }
 
     public class SonarrService : ArrService, ISonarrService
@@ -38,6 +40,38 @@ namespace Gatarr.Services
             series = await response.Content.ReadFromJsonAsync<List<Series>>();
 
             return series ?? new List<Series>();
+        }
+
+        public async Task<Series> GetSeries(string id)
+        {
+            var series = new Series();
+
+            var response = await HttpClient.GetAsync($"series/{id}");
+
+            if (response?.Content == null || !response.IsSuccessStatusCode)
+            {
+                return series;
+            }
+
+            series = await response.Content.ReadFromJsonAsync<Series>();
+
+            return series ?? new Series();
+        }
+
+        public async Task<List<Episode>> GetEpisodes(string id)
+        {
+            var episodes = new List<Episode>();
+
+            var response = await HttpClient.GetAsync($"episode?seriesId={id}");
+
+            if (response?.Content == null || !response.IsSuccessStatusCode)
+            {
+                return episodes;
+            }
+
+            episodes = await response.Content.ReadFromJsonAsync<List<Episode>>();
+
+            return episodes ?? new List<Episode>();
         }
     }
 }
