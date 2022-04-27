b670d400a7acb349760c16586873e484f20880bf
mcbtay <mcbtay@gmail.com>
2022-04-26 21:03:27 -0400

    adding constants for reusable strings/icon names

---
+++ Gatarr/Models/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace Gatarr.Models
+{
+    public class EventTypeConstants
+    {
+        public const string DownloadFailed = "downloadFailed";
+        public const string DownloadFolderImported = "downloadFolderImported";
+        public const string DownloadIgnored = "downloadIgnored";
+        public const string Grabbed = "grabbed";
+        public const string MovieFileDeleted = "movieFileDeleted";
+        public const string MovieFileRenamed = "movieFileRenamed";
+        public const string MovieFolderImported = "movieFolderImported";
+    }
+
+    public class FontAwesomeConstants
+    {
+        public const string CircleQuestion = "fa-circle-question";
+        public const string CircleXMark = "fa-circle-xmark";
+        public const string CloudArrowDown = "fa-cloud-arrow-down";
+        public const string Download = "fa-download";
+        public const string Hdd = "fa-hdd";
+        public const string Sitemap = "fa-sitemap";
+        public const string TrashCan = "fa-trash-can";
+    }
+}
