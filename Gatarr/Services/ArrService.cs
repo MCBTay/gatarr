5fc8fad845d4dc320dd017195db7d586b26d42d3
mcbtay <mcbtay@gmail.com>
2022-04-30 21:47:43 -0400

    adding IArrService, updating ArrService to have a method to change the properties of the HttpClient as well as an abstract definition for a method to test the connection

--- Gatarr/Services/ArrService.cs
+++ Gatarr/Services/ArrService.cs
@@ -1,31 +1,58 @@
 ﻿namespace Gatarr.Services
 {
-    public class ArrService
+    public interface IArrService
     {
+        string Name { get; set; }
+        string Host { get; set; }
+        string Port { get; set; }
+        string ApiKey { get; set; }
+
+        void ConfigureHttpClient(string? host, string? port, string? apiKey, bool? useHttps);
+        Task<bool> TestConnection();
+    }
+
+    public abstract class ArrService : IArrService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
         protected readonly IConfiguration Configuration;
-        protected readonly HttpClient HttpClient;
+        protected HttpClient HttpClient;
 
-        protected readonly string Name;
-
-        protected readonly string Host;
-        protected readonly string Port;
-        protected readonly string ApiKey;
+        public string Name { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string ApiKey { get; set; }
 
         protected ArrService(
             IConfiguration configuration,
             IHttpClientFactory httpClientFactory,
             string name)
         {
+            _httpClientFactory = httpClientFactory;
+
             Configuration = configuration;
             Name = name;
-
-            Host = Configuration[$"{Name}:Host"];
-            Port = Configuration[$"{Name}:Port"];
-            ApiKey = Configuration[$"{Name}:ApiKey"];
-
-            HttpClient = httpClientFactory.CreateClient();
-            HttpClient.BaseAddress = new Uri($"http://{Host}:{Port}/api/v3/");
-            HttpClient.DefaultRequestHeaders.Add("X-Api-Key", ApiKey);
+            
+            ConfigureHttpClient();
         }
+
+        public void ConfigureHttpClient(string? host = null, string? port = null, string? apiKey = null, bool? useHttps = null)
+        {
+            Host = host ?? Configuration[$"{Name}:Host"];
+            Port = port ?? Configuration[$"{Name}:Port"];
+            ApiKey = apiKey ?? Configuration[$"{Name}:ApiKey"];
+
+            bool.TryParse(Configuration[$"{Name}:UseHttps"], out var _useHttpsConfig);
+            var useHttpsFinal = useHttps ?? _useHttpsConfig;
+            var protocol = useHttpsFinal ? "https" : "http";
+
+            HttpClient = _httpClientFactory.CreateClient();
+
+            HttpClient.BaseAddress = new Uri($"{protocol}://{Host}:{Port}/api/v3/");
+            HttpClient.DefaultRequestHeaders.Add("X-Api-Key", ApiKey);
+            HttpClient.Timeout = TimeSpan.FromSeconds(30);
+        }
+
+        public abstract Task<bool> TestConnection();
     }
 }
