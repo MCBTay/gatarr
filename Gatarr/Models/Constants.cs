077c9a699b2c281c6141b03b1c6dab357955c48a
mcbtay <mcbtay@gmail.com>
2022-05-01 13:47:40 -0400

    updating any FA usage to use the constants class

--- Gatarr/Models/Constants.cs
+++ Gatarr/Models/Constants.cs
@@ -13,16 +13,26 @@
 
     public class FontAwesomeConstants
     {
+        public const string Check = "fa-check";
+        public const string CircleExclamation = "fa-circle-exclamation";
         public const string CircleQuestion = "fa-circle-question";
         public const string CircleXMark = "fa-circle-xmark";
         public const string ClockRotateLeft = "fa-clock-rotate-left";
         public const string CloudArrowDown = "fa-cloud-arrow-down";
+        public const string CompactDisc = "fa-compact-disc";
         public const string Download = "fa-download";
         public const string FileVideo = "fa-file-video";
+        public const string Film = "fa-film";
+        public const string Gear = "fa-gear";
         public const string Hdd = "fa-hdd";
+        public const string House = "fa-house";
         public const string MagnifyingGlass = "fa-magnifying-glass";
+        public const string Music = "fa-music";
+        public const string Save = "fa-save";
         public const string Sitemap = "fa-sitemap";
+        public const string Ticket = "fa-ticket";
         public const string TrashCan = "fa-trash-can";
+        public const string TV = "fa-tv";
         public const string User = "fa-user";
     }
 }
